650ae6d4420e697cde34e5056eb3f3b229680922
niko <60353265+nurullahguc@users.noreply.github.com>
2020-03-26 14:52:16 +0300

    FirstUpluad

---
+++ YoutubeMp3Downolader/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace YoutubeMp3Downolader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelHeader = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButtonMP3 = new System.Windows.Forms.RadioButton();
+            this.radioButtonMP4 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelDurum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelHeader
+            // 
+            this.labelHeader.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHeader.ForeColor = System.Drawing.Color.Blue;
+            this.labelHeader.Location = new System.Drawing.Point(21, 9);
+            this.labelHeader.Name = "labelHeader";
+            this.labelHeader.Size = new System.Drawing.Size(533, 23);
+            this.labelHeader.TabIndex = 0;
+            this.labelHeader.Text = "Youtube MP3/MP4 Downloader";
+            this.labelHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Video URL :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(53, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Format :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(134, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(429, 29);
+            this.textBox1.TabIndex = 3;
+            // 
+            // radioButtonMP3
+            // 
+            this.radioButtonMP3.AutoSize = true;
+            this.radioButtonMP3.Checked = true;
+            this.radioButtonMP3.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonMP3.Location = new System.Drawing.Point(143, 93);
+            this.radioButtonMP3.Name = "radioButtonMP3";
+            this.radioButtonMP3.Size = new System.Drawing.Size(67, 27);
+            this.radioButtonMP3.TabIndex = 4;
+            this.radioButtonMP3.TabStop = true;
+            this.radioButtonMP3.Text = "MP3";
+            this.radioButtonMP3.UseVisualStyleBackColor = true;
+            this.radioButtonMP3.CheckedChanged += new System.EventHandler(this.radioButtonMP3_CheckedChanged);
+            // 
+            // radioButtonMP4
+            // 
+            this.radioButtonMP4.AutoSize = true;
+            this.radioButtonMP4.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonMP4.Location = new System.Drawing.Point(216, 93);
+            this.radioButtonMP4.Name = "radioButtonMP4";
+            this.radioButtonMP4.Size = new System.Drawing.Size(67, 27);
+            this.radioButtonMP4.TabIndex = 5;
+            this.radioButtonMP4.Text = "MP4";
+            this.radioButtonMP4.UseVisualStyleBackColor = true;
+            this.radioButtonMP4.CheckedChanged += new System.EventHandler(this.radioButtonMP4_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(335, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(228, 38);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelDurum
+            // 
+            this.labelDurum.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDurum.Location = new System.Drawing.Point(12, 147);
+            this.labelDurum.Name = "labelDurum";
+            this.labelDurum.Size = new System.Drawing.Size(542, 23);
+            this.labelDurum.TabIndex = 7;
+            this.labelDurum.Text = "-";
+            this.labelDurum.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 179);
+            this.Controls.Add(this.labelDurum);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButtonMP4);
+            this.Controls.Add(this.radioButtonMP3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelHeader);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelHeader;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButtonMP3;
+        private System.Windows.Forms.RadioButton radioButtonMP4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelDurum;
+    }
+}
+
